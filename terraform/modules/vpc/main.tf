f092018fb081decedf9050242bd61592a5410527
AnginAbroyan <anginabroyan@gmail.com>
2024-10-13 19:52:22 +0400

    Updated vpc nat

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -75,7 +75,7 @@ resource "aws_route_table_association" "private_rt_assoc" {
 resource "aws_eip" "nat_eip" {
 #  count  = length(var.private_subnet_cidr)
   domain = "vpc"
-  tags   = merge(var.tags, { Name = "${var.project_name}-nat-gtw" })
+  tags   = merge(var.tags, { Name = "${var.project_name}-nat-gtw-ip" })
 }
 
 
@@ -84,6 +84,6 @@ resource "aws_nat_gateway" "nat_gtw" {
 #  count         = length(var.private_subnet_cidr)
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.public_subnet[1].id
-  tags          = merge(var.tags, { Name = "${var.project_name}-nat-gtw-${count.index + 1}" })
+  tags          = merge(var.tags, { Name = "${var.project_name}-nat-gtw" })
   depends_on    = [aws_internet_gateway.internet_gtw]
 }
