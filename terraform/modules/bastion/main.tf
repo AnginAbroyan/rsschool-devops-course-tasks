ee4bd914395d550810f30f94215975f4bd193474
AnginAbroyan <anginabroyan@gmail.com>
2024-10-13 19:37:19 +0400

    Updated bastion

--- terraform/modules/bastion/main.tf
+++ terraform/modules/bastion/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "bastion" {
   ami              = var.bastion_ami
   instance_type    = var.bastion_instance_type
-  public_subnet_id = var.subnet_id
+  subnet_id = var.subnet_id
   tags             = merge(var.tags, { Name = "${var.project_name}-bastion" })
   key_name         = var.ssh_key_name
 }
